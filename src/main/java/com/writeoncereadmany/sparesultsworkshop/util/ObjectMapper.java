ec343e72551f2cf45de5367d91c5ffa5f985efd1
tom <tarjohnson1979@gmail.com>
2018-06-06 16:47:36 +0100

    Starting point for the Failure Patterns workshop

--- src/main/java/com/writeoncereadmany/sparesultsworkshop/util/ObjectMapper.java
+++ src/main/java/com/writeoncereadmany/sparesultsworkshop/util/ObjectMapper.java
@@ -1,11 +1,8 @@
 package com.writeoncereadmany.sparesultsworkshop.util;
 
-import co.unruly.control.result.Result;
 import com.writeoncereadmany.sparesultsworkshop.domain.Enquiry;
 
 public interface ObjectMapper {
 
     Enquiry readObject(String representation);
-
-    Result<Enquiry, String> readObject2(String representation);
 }
